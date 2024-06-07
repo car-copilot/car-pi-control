a07483b54297b6bb4059738f61aa8a407b792e23
Samuel Rodrigues <sam200133@gmail.com>
2024-06-07 21:19:40 +0200

    init

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"net"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/rs/zerolog/log"
+)
+
+func get_battery_power_plugged() bool {
+	connection, err := net.Dial("tcp", "192.168.1.102:8423")
+	if err != nil {
+		log.Error().Msg("Error connecting to server")
+	}
+	defer connection.Close()
+
+	_, err = connection.Write([]byte("get battery_power_plugged"))
+	if err != nil {
+		panic(err)
+	}
+	buffer := make([]byte, 128)
+	mLen, err := connection.Read(buffer)
+	if err != nil {
+		log.Error().Msg("Error reading from server")
+	}
+	out := strings.TrimSpace(string(buffer[:mLen]))
+	out = out[len(out)-4:]
+	log.Debug().Msgf("Battery power plugged: %s", out)
+	return string(out) == "true"
+
+}
+
+func main() {
+	defaultTimer := 1 * time.Minute
+	timer := defaultTimer
+	for {
+		if connected := get_battery_power_plugged(); connected {
+			if timer != defaultTimer {
+				log.Info().Msg("Connected to power source")
+				log.Info().Msg("Resetting timer")
+				timer = defaultTimer
+			}
+			time.Sleep(10 * time.Second)
+		} else {
+			log.Info().Msgf("Shuting down in %s", timer)
+			if timer < (5 * time.Second) {
+				log.Info().Msg("Shutdown initiated")
+				// Shutdown the computer
+				cmd := exec.Command("shutdown", "now")
+				err := cmd.Run()
+				if err != nil {
+					log.Error().Msg("Error shutting down computer")
+				}
+			}
+			timer = timer - 10*time.Second
+			time.Sleep(10 * time.Second)
+		}
+	}
+}
