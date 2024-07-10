8751fcb1d011962cef1f69b6681a8810dcf55bc3
obito1903 <sam200133@gmail.com>
2024-07-10 20:47:02 +0200

    add card config

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"net/url"
@@ -57,6 +59,38 @@ func set_volume(volume int) {
 
 }
 
+func set_config(card int) {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	var data = strings.NewReader(fmt.Sprintf("output_device_cardnum=%d&update_output_device=novalue&mixer_type=null&camilladsp_volume_range=60&i2sdevice=None&i2soverlay=None&drvoptions=none&autoplay=1&extmeta=0&ashufflesvc=0&ashuffle_mode=Track&ashuffle_window=7&ashuffle_filter=None&volume_step_limit=5&volume_mpd_max=100&volume_db_display=1&usb_volknob=0&rotaryenc=0&rotenc_params=100+2+3+23+24&mpdcrossfade=0&mpd_httpd=0&mpd_httpd_port=8000&mpd_httpd_encoder=lame&cdsp_mode=Audi.yml", card)
+	req, err := http.NewRequest("POST", "http://"+*address+"/snd-config.php", data)
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+	req.Header.Set("Accept-Language", "en-US,en;q=0.9")
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Cookie", "PHPSESSID=ho7vk67sqrjua8sme0pqhsjgdq")
+	req.Header.Set("Origin", "http://"+*address)
+	req.Header.Set("Pragma", "no-cache")
+	req.Header.Set("Referer", "http://"+*address+"/snd-config.php")
+	req.Header.Set("Upgrade-Insecure-Requests", "1")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+}
+
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout}).Level(zerolog.InfoLevel)
@@ -68,6 +102,7 @@ func main() {
 
 	go func() {
 		time.Sleep(20 * time.Second)
+		set_config(0)
 		set_volume(98)
 	}()
 
